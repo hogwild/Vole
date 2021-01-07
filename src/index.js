ee5106e79968c725601c375efeae6ed76e7b5dcf
Xianbin Gu <xianbingu@XianbindeiMac.local>
2021-01-07 22:57:09 +0800

    gui complete

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import './index.css';
 class CodeInput extends React.Component {
   render() {
     return (
-      <div>
+      <div className='program'>
       <h1>Program</h1>
       <form>
       {/* <label>
         Name:
         <input type='text' name='name' />
       </label> */}
-      <textarea rows='40' cols='50'>
+      <textarea rows='40' cols='40'>
         Input machine instructions here.
       </textarea>
       </form>
@@ -26,38 +26,65 @@ class Cell extends React.Component {
 
   render() {
     return (
-      <div>
-        <p>{this.props.name}</p>
-      </div>
+        <div className='cell'>
+        <button className='index'>
+          {this.props.index}
+        </button>
+        <button className='content'>
+          {this.props.value}
+        </button>
+        </div>
     )
   }
 }
 
 class Registers extends React.Component {
-
   render(){
     let i;
     let registers = [];
-    for (i = 0; i < 16; i++ ) {
-      registers.push(<Cell name='register'/>);
+    for (i = 0; i < this.props.num; i++ ) {
+      registers.push(<Cell index={i.toString(16).toUpperCase()} value='AF'/>);
     };
-    return (
-      <div>
-        {registers}
-      </div>
-    )
+    if(this.props.type === 'register') {
+      return (
+        <div className='registers'>
+          <h1>Registers</h1>
+          {registers}
+        </div>
+      )
+    }else{
+      if(this.props.type === 'pc'){
+      return (
+        <div className='pc'>
+          <h4>Program counter</h4>
+          <Cell index='PC' value='F8'/>
+        </div>
+      )
+      }else{
+        if(this.props.type === 'ir'){
+          return (
+            <div className='ir'>
+              <h4>Instruction Registor</h4>
+              <Cell index='IR' value='AFF8'/>
+          </div>
+          )
+        }
+      }
+    }
   }
 }
+
 
 class RAM extends React.Component {
   render() {
     let i = 0;
     let ram = [];
-    for(i=0; i<256; i++){
-      ram.push(<Cell name='RAM'/>);
+    for(i=0; i<this.props.num; i++){
+      ram.push(<Cell index={i.toString(16).toUpperCase()} value='FF'/>);
     };
   return (
-    <div>
+    <div className='ram'>
+      <h1>Main Memory</h1>
       {ram}
     </div>
     )
@@ -70,10 +97,12 @@ class Vole extends React.Component {
       <div>
         <h1>Vole: a simplified computer</h1>
         <CodeInput />
-        <h1>Registers</h1>
-        <Registers />
-        <h1>RAM</h1>
-        <RAM />
+        {/* <h1>Registers</h1> */}
+        <Registers num='16' type='register'/>
+        <Registers num='1' type='pc'/>
+        <Registers num='1' type='ir'/>
+        {/* <h1>RAM</h1> */}
+        <RAM num='256'/>
       </div>
       
     )
