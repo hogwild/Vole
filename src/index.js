756104a9a09171552980647c3f1cc2ddd8010e41
Xianbin Gu <xianbingu@XianbindeiMac.local>
2021-01-07 10:29:29 +0800

    ver1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class CodeInput extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Program</h1>
+      <form>
+      {/* <label>
+        Name:
+        <input type='text' name='name' />
+      </label> */}
+      <textarea rows='40' cols='50'>
+        Input machine instructions here.
+      </textarea>
+      </form>
+      </div>      
+    )
+  }
+}
+
+class Cell extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+      </div>
+    )
+  }
+}
+
+class Registers extends React.Component {
+
+  render(){
+    let i;
+    let registers = [];
+    for (i = 0; i < 16; i++ ) {
+      registers.push(<Cell name='register'/>);
+    };
+    return (
+      <div>
+        {registers}
+      </div>
+    )
+  }
+}
+
+class RAM extends React.Component {
+  render() {
+    let i = 0;
+    let ram = [];
+    for(i=0; i<256; i++){
+      ram.push(<Cell name='RAM'/>);
+    };
+  return (
+    <div>
+      {ram}
+    </div>
+    )
+  }
+}
+
+class Vole extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Vole: a simplified computer</h1>
+        <CodeInput />
+        <h1>Registers</h1>
+        <Registers />
+        <h1>RAM</h1>
+        <RAM />
+      </div>
+      
+    )
+  }
+}
+
+ReactDOM.render(<Vole />, document.getElementById('root'))
