07e585fd1dabac0a0ced22c058567f89b56aed53
Xianbin Gu <xianbingu@XianbindeiMac.local>
2021-01-08 23:59:59 +0800

    adding execution functions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { run } from './run.js';
 
+class CodeForm extends React.Component {
+  constructor(props){
+    super(props);
+    // this.state = {
+    //   value:'Input your program here.',
+    // };
+    // this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.input = React.createRef();
+  }
+  // handleChange(event) {
+  //   this.setState({
+  //     value: event.target.value
+  //   });
+  // }
 
-class CodeInput extends React.Component {
+  handleSubmit(event) {
+    {console.log(this.input.current.value.split(" "))};
+    alert('A program was submitted: '+ this.input.current.value);
+    event.preventDefault();
+    
+  }
+
   render() {
+    // {console.log(this.input.current.value)};
     return (
       <div className='program'>
         <h1>Program</h1>
-        <form>
-          <textarea rows='40' cols='40'>
-            Input machine instructions here.
-          </textarea>
-        </form>
-        <button>
+        <form onSubmit={this.handleSubmit}>
+          {/* <textarea rows='40' cols='40' value={this.state.value} onChange={this.handleChange}/> */}
+          <textarea rows='40' cols='40' placeholder='Please input your code here.' ref={this.input}/>
+          <button>
           Run
         </button>
+        </form>
       </div>      
     )
   }
 }
+
 
 class Cell extends React.Component {
 
@@ -97,7 +120,7 @@ class Vole extends React.Component {
     return (
       <div>
         <h1>Vole: a simplified computer</h1>
-        <CodeInput />
+        <CodeForm />
         {/* <h1>Registers</h1> */}
         <Registers num='16' type='register'/>
         <Registers num='1' type='pc'/>
